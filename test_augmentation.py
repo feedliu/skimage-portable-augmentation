ae751c44ce844d1ead0951beba48ca4a61927877
feedliu <42089226+feedliu@users.noreply.github.com>
2018-12-19 15:52:38 +0800

    remove redundant info

--- test_augmentation.py
+++ test_augmentation.py
@@ -1,12 +1,8 @@
-import logging
 from image_augmentation import ImageAugmentor
 
 if __name__ == "__main__":
-    logging.basicConfig(format='%(asctime)s %(levelname)s : %(message)s',
-                        datefmt='%Y/%m/%d %H:%M:%S',
-                        level=logging.DEBUG)
     # if you want to put new images in the in_folder, just ignore out_folder parameter
-    #augmentor = ImageAugmentor("/home/feedliu/Documents/skimage-portable-augmentation/images")
+    #augmentor = ImageAugmentor("./images")
     augmentor = ImageAugmentor("./images",
                                out_folder="./augmentation", num_thread=8, extension="jpg")
     augmentor.augmentation()
